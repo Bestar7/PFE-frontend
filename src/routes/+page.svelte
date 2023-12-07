b2cc0aacf56d53121817a6b36d9525977885206b
Your Name <quentin.garwig@student.vinci.be>
2023-12-07 10:35:32 +0100

    Ajout des routes pour les livreurs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,5 +14,6 @@
 <nav>
     <a href="/api/creche">creche</a><br>
     <a href="/api/connexion">connexion</a><br>
+    <a href="livreurs"> Livreurs</a><br> 
 </nav>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/livreurs/[livreur]/+page.server.js
@@ -0,0 +1,6 @@
+import {error} from '@sveltejs/kit';
+import {livreurs} from '../data';
+
+
+
+
