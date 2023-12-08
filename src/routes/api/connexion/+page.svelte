087eceb1e0ecb2a5115133ba64a671fe244aca02
JO@IPL <joachim.bastin@student.vinci.be>
2023-12-08 12:02:05 +0100

    refactor add full connexion form

--- src/routes/api/connexion/+page.svelte
+++ src/routes/api/connexion/+page.svelte
@@ -17,8 +17,26 @@
     document.cookie = `auth=${cookiedUser} ;` // TODO rename?/export (auth) (in connexion/+page.svelte too)
     console.log(`Connecting as ${cookiedUser}`, document.cookie)
   }
+
+  function submitConnexionForm(){
+    console.log("form submitted") // TODO get values, send value rememberMe ?? cookie=... : void
+  }
 </script>
 
+<form on:submit={submitConnexionForm}>
+  <label for="email">Email</label>
+  <input type="email" id="email" required>
+
+  <label for="password">Mot de passe</label>
+  <input type="password" id="password" required>
+
+  <label for="rememberMe">Se souvenir de moi</label>
+  <input type="checkbox" id="rememberMe">
+
+  <button type="submit">Connexion</button>
+</form>
+
+<br><br><br><br>
 <div>
   {#each usersTest as user}
     <User {...user} connectAs={() => connect(user.name)}/>
@@ -28,5 +46,5 @@
 {#if document.cookie != ""} <!--TODO check this cndt (like useState) ex right after connecting-->
   <button on:click={ logOut }>logOut</button>
 {/if}
-<button on:click={() => history.back() }>-&gt; Index</button>
+<button on:click={() => history.back() }>Retour</button>
 <button on:click={() => console.log(document.cookie) }>COOKIE</button>
