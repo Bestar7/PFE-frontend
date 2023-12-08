88f113554e98d2be57205d15f75fb6bb624b3062
JO@IPL <joachim.bastin@student.vinci.be>
2023-12-08 10:42:43 +0100

    add tournee

---
+++ src/lib/Components/Tournee.svelte
@@ -0,0 +1,22 @@
+<script>
+    // TODO renommé le fichier pour differencier de la route /tournee ???
+    
+    /**
+     * @type {string}
+     */
+    export let nomTournee;
+     /**
+     * @type {string}
+     */
+    export let livreur;
+
+    /**
+     * @type {() => void}
+    */
+    export let supprimerTournee;
+</script>
+
+<div class="tournee ligneItem">
+    <h3>Tournée : {nomTournee}</h3>
+    <p>Livreur : {livreur} <button on:click={supprimerTournee}>x</button></p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-    import HelloWorld from "$lib/Components/HelloWorld.svelte";
     import { onMount } from "svelte";
     
     let name = "loadin...";
@@ -10,9 +9,9 @@
     })
 </script>
 
-<HelloWorld {name}/>
 <nav>
     <a href="/api/creche">creche</a><br>
     <a href="/api/connexion">connexion</a><br>
+    <a href="/api/tournee">tournee</a><br>
 </nav>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
