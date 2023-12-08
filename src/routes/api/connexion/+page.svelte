77f4cfc2bcef0e8b421037333c25a917e2833f19
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-08 22:58:17 +0100

    add auth in cookie + update of HTML based on auth->true/false

--- src/routes/api/connexion/+page.svelte
+++ src/routes/api/connexion/+page.svelte
@@ -1,24 +1,10 @@
 <script>
-  import User from "$lib/Components/User.svelte";
-  //import { writable } from "svelte/store";
+  import {resetAuth, setAuth, authStore} from "$lib/Auth/auth"
+  $: auth = $authStore != ""
 
-  // TODO : GET /users THEN sort by Admin first then delivery
-  const usersTest = [{name:"Admin", role:"admin"},{name:"Tom", role:"delivery"},{name:"Fred", role:"delivery"}]
-
-  //$: obsCookie = document.cookie // TODO update the HTML with changes to the cookie
-  //let obsCookie2 = writable(obsCookie)
-
-  function logOut(){
-    document.cookie = "auth=e ; Max-Age=-99999999;"// TODO rename?/export (auth) (in User.svelte too)
+  function logout(){
+    resetAuth()
     history.back()
-  }
-
-  /**
-   * @param {string} cookiedUser 
-   */
-   function connect(cookiedUser){ // TODO id instead ? what about admin (not in DB)?
-    document.cookie = `auth=${cookiedUser} ;` // TODO rename?/export (auth) (in connexion/+page.svelte too)
-    console.log(`Connecting as ${cookiedUser}`, document.cookie)
   }
 
   /** @type {string} */
@@ -27,12 +13,13 @@
   let password
   /** @type {boolean} */
   let rememberMe // TODO ne marche pas, genre pas un boolean ??
-  function submitConnexionForm(){
+  function login(){
+    setAuth(email)// TODO take the appropriate field
     console.log("FIELDS : ", email, password, rememberMe)
   }
 </script>
 
-<form on:submit|preventDefault={submitConnexionForm}>
+<form on:submit|preventDefault={login}>
   <label for="email">Email</label>
   <input type="email" id="email" bind:value={email} required>
 
@@ -45,15 +32,7 @@
   <button type="submit">Connexion</button>
 </form>
 
-<br><br><br><br>
-<div>
-  {#each usersTest as user}
-    <User {...user} connectAs={() => connect(user.name)}/>
-  {/each}
-</div><br>
-
-{#if document.cookie != ""} <!--TODO check this cndt (like useState) ex right after connecting-->
-  <button on:click={ logOut }>logOut</button>
+{#if auth} <!--TODO check this cndt (like useState) ex right after connecting-->
+  <button on:click={ logout }>Se déconnecter</button>
 {/if}
 <button on:click={() => history.back() }>Retour</button>
-<button on:click={() => console.log(document.cookie) }>COOKIE</button>
