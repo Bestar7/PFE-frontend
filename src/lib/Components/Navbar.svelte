363ddab75edbb505ca9daea8ca929d5bff6a26cb
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-09 11:33:11 +0100

    add Navbar component

---
+++ src/lib/Components/Navbar.svelte
@@ -0,0 +1,7 @@
+<nav>
+  <a href="/">index</a><br>
+  <a href="/api/creche">creche</a><br>
+  <a href="/api/connexion">connexion</a><br>
+  <a href="/api/tournee">tournee</a><br>
+  <a href="livreurs"> Livreurs</a><br> 
+</nav>
