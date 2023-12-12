e91707c7f83f089035c7636365187227d6f6e654
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-12 17:09:47 +0100

    refactor move files

--- src/lib/Components/Navbar.svelte
+++ src/lib/Components/Navbar.svelte
@@ -1,10 +1,8 @@
 <nav>
   <a href="/">Index</a><br>
-  <a href="/api/creche">creche</a><br>
-  <a href="/api/connexion">connexion</a><br>
+  <a href="/creches">Creches</a><br>
+  <a href="/connexion">Connexion</a><br>
   <a href="/tournees">Tournee</a><br>
   <a href="/livreurs">Livreurs</a><br> 
   <a href="/supplements">Supplements</a><br> 
-
-
 </nav>
