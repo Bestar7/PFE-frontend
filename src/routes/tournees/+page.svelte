b2beebff82f160b3521573bfca019150c1c5e43c
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-12 18:09:13 +0100

    revert back to deleted code ???

--- src/routes/tournees/+page.svelte
+++ src/routes/tournees/+page.svelte
@@ -1,218 +1,103 @@
 <script>
   import Navbar from "$lib/Components/Navbar.svelte";
-  import Tournee from "$lib/Components/Tournee.svelte";
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
-  let tournees = [];
 
   /**
-   * @param {number} id
+   * @typedef {import("$lib/Model/Tournee").Tournee} Tournee
    */
-  function supprimerTournee(id) {
-    testListTournee = testListTournee.filter((tournee) => tournee.id !== id);
-  }
-  onMount(() => {
-    getTourneesDate();
-    console.log(tournees);
-  });
-
-  let currentDate = new Date();
-  let dateString = currentDate.toISOString().split("T")[0];
-  console.log("date ", dateString);
-  let datePicked =dateString;
-
-
-  async function getTourneesDate() {
-    const response = await fetch(`/api/tournees/date/${datePicked}`);
-    console.log(response)
-    const tourneesResponse = await response.json();
-    tournees = tourneesResponse;
-  }
-  function handleDateChange(event) {
-    currentDate = new Date(event.target.value);
-    datePicked = currentDate.toISOString().split("T")[0];
-    console.log("nouvelle date ; ", datePicked);
-    getTourneesDate();
-  }
-
-  /**
-   * affiche la liste des differentes commandes presentes dans la tournes (le nom des creches a livrer dans l'ordre )
-   */
-
-  /**
-   * @type {string}
-   */
-
-  function handleDateSearch() {
-    getTourneesDate();
-    // TODO search tournee for this date GET /Tournee/{date}
-
-    selectedTab = tabs.TourneeDate;
-  }
-  function handleDefault() {
-    // TODO search tournee for this date GET /Tournee/{Date.Now()}
-    selectedTab = tabs.TourneeDefault;
-    console.log("selectedTab", selectedTab);
-  }
-
+  
   const tabs = {
     TourneeDate: "TourneeDate",
     TourneeDefault: "TourneeDefault",
     TourneeSupplement: "TourneeSupplement",
   };
-  function eventHandler(id_tournee) {
-    sessionStorage.setItem("idTournee", id_tournee);
-    console.log("ici", sessionStorage.getItem("idTournee"));
-    goto(`/tournees/${id_tournee}`);
-  }
   let selectedTab = tabs.TourneeDate;
+  let datePicked = new Date().toLocaleDateString("en-CA"); // = date.now() avec format YYY-MM-DD comme l'input du form
 
-  let testListTournee = [
-    // TODO GET THIS LIST FROM THE DATABASE
-    // TODO changes depending on the selected tab
-    { id: 1, nomTournee: "BXL", livreur: "Tom" },
-    { id: 2, nomTournee: "LLN", livreur: "Fred" },
-    { id: 3, nomTournee: "Leuven", livreur: "Paul" },
-  ];
+  /** @type {Tournee[]} */
+  let tournees = [];
+  onMount(() => {
+    getTourneesDate();
+    console.log("getTourneesDate", tournees);
+  });
+
+  async function getTourneesDate() {
+    const response = await fetch(`/api/tournees/date/${datePicked}`);
+    tournees = await response.json();
+  }
+  async function getTourneesDefault() {
+    const response = await fetch(`/api/tournees/default/${datePicked}`); //TODO verifier path
+    tournees = await response.json();
+  }
+
+  function handleDateChange() {
+    getTourneesDate();
+    selectedTab = tabs.TourneeDate;
+    console.log("tournee", tournees)
+  }
+  function handleDefault() {
+    getTourneesDefault();
+    selectedTab = tabs.TourneeDefault;
+  }
+
+  /**
+   * @param {Tournee} tournee
+   */
+  function eventHandler(tournee) {
+    sessionStorage.setItem("idTournee", tournee.id_tournee);
+    console.log("ici", sessionStorage.getItem("idTournee"));
+    goto(`/tournees/${tournee.id_tournee}`);
+  }
 </script>
 
-<div class="container">
-  <Navbar />
-  <!--TODO au lieu de mettre dans chaque pages, le mettre UNE fois dans le +- main-->
+<Navbar />
+<div class="container"><!--TODO au lieu de mettre dans chaque pages, le mettre UNE fois dans le +- main-->
   <div class="centered">
-    <div>
-      <div>
-        <form>
-          <input
-            type="date"
-            name="dateTournee"
-            on:change={handleDateChange}
-            bind:value={datePicked}
-          />
-        </form>
-      </div>
-      <div class="button">
-        <!--TODO Button or a:link ??-->
-        <button on:click={handleDefault}>Tournées par défaut</button>
-      </div>
-
+    <div class="tab-selection">
+      <input type="date" on:change={handleDateChange} bind:value={datePicked} />
+      <button on:click={handleDefault}>Tournées par défaut</button>
+    </div>
+    <div class="show-tournee">
       <h2>Tournée du {datePicked}</h2>
-      
-      {#each tournees as tournee (tournee)}
-        <!-- warning car il n'aime pas que un div soit clickable, ok si c est un button mais alors il faut modifier CSS-->
-        <table
-          class="tab-infos"
-          on:click={() => eventHandler(tournee.id_tournee)}
-        >
-          <!-- TODO affihcer la bonne nouvelle page /tournees/id-->
-          <div class="left-column">
-            <ul class="flex-container">
-              <label for="nom"> Tournee : </label>
-              <span id="nom"> {tournee.nom}</span>
-            </ul>
-            <ul class="flex-container">
-              <label for="livreur"> Livreur : </label>
-              <span id="livreur"> {tournee.prenom_livreur}</span>
-            </ul>
-          </div>
-          <div class="left-column">
-            <ul class="flex-container">
-              <label for="date"> Date : </label>
-              <span id="date"> {tournee.date}</span>
-            </ul>
-            <ul class="flex-container">
-              <label for="statut"> Statut : </label>
-              <span id="statut">{tournee.statut}</span>
-            </ul>
-          </div>
-        </table>
-      {/each}
+      <table>
+        <tbody>
+          {#each tournees as tournee}
+          <tr on:click={()=>eventHandler(tournee)} class="tab-infos">
+            <td>{tournee.prenom_livreur} {tournee.nom_livreur}</td>
+            <td>{tournee.date}</td>
+            <td>{tournee.nom}</td>
+            <td>{tournee.statut}</td>
+          </tr>
+          {/each}
+        </tbody>
+      </table>
     </div>
   </div>
   <button on:click={() => history.back()}>Retour</button>
 </div>
-<foot>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Formulaire avec mise en page</title>
 
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      display: flex;
-      justify-content: center;
-      height: 100vh;
-      align-items: center;
-      margin: 0;
-    }
 
-    .container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    }
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-    .tab-infos {
-      border: 1px solid #ddd; /* Ajoute une bordure autour du conteneur */
-      padding: 10px; /* Ajoute de l'espace intérieur pour le contenu */
-      margin-bottom: 10px;
-      background-color: #f2f2f2; /* Ajoute une couleur de fond grise */
+  .tab-infos {
+    border: 1px solid #ddd; /* Ajoute une bordure autour du conteneur */
+    padding: 10px; /* Ajoute de l'espace intérieur pour le contenu */
+    margin-bottom: 10px;
+    background-color: #f2f2f2; /* Ajoute une couleur de fond grise */
+    display: flex;
+    justify-content: space-between;
+    width: 500px;
+  }
 
-      display: flex;
-      justify-content: space-between;
-      width: 500px;
-    }
-
-    .left-column,
-    .right-column {
-      width: 100%;
-      margin-top: 10px;
-    }
-
-    .flex-container {
-      display: flex;
-      justify-content: space-between;
-    }
-
-    .data-table {
-      margin-bottom: 20px;
-    }
-
-    table {
-      border-collapse: collapse;
-      width: 400px;
-      margin: 20px;
-    }
-
-    th,
-    td {
-      padding: 10px;
-      border: 1px solid #ddd;
-    }
-
-    th {
-      background-color: #333;
-      color: #fff;
-    }
-
-    tr:nth-child(even) {
-      background-color: #f2f2f2;
-    }
-
-    .buttons {
-      display: flex;
-      justify-content: space-around;
-      margin-top: 20px;
-    }
-
-    .date-input {
-      width: 100%;
-    }
-
-    label,
-    span {
-      display: inline-block;
-      margin-bottom: 8px;
-    }
-  </style>
-</foot>
+  table {
+    border-collapse: collapse;
+    width: 400px;
+    margin: 20px;
+  }
+</style>
