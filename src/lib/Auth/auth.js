e3017b814d7aa7c299b0a4c7ba5c7e9968dd0641
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-13 22:28:12 +0100

    fix error putting object in local/sessionStorage

--- src/lib/Auth/auth.js
+++ src/lib/Auth/auth.js
@@ -1,19 +1,28 @@
-// TODO + de test (console.log du contenu du cookie) + que se passe-il si on a plusieurs cookies ??? dans connexion
-// TODO use localStorage && sessionStorage
 import { writable } from 'svelte/store';
 
 let auth = writable({})
 
 const getAuth = () => {
-  return localStorage.getItem("auth") ?? sessionStorage.getItem("auth");
+  if (localStorage.getItem("auth") != null){
+    console.log("localStorage", localStorage.getItem("auth"))
+    return localStorage.getItem("auth")
+  }
+    
+  if (sessionStorage.getItem("auth") != null){
+    console.log("sessionStorage", sessionStorage.getItem("auth"))
+    return sessionStorage.getItem("auth")
+  }
+  return {} //TODO handle this case (happen right after disconnect)
 }
 
 const setAuth = (authValue, rememberMe) => {
   auth.set(authValue)
   if (rememberMe)
-    localStorage.setItem("auth", authValue)
-  else
-    sessionStorage.setItem("auth", authValue)
+    localStorage.setItem("auth", authValue.role)
+  else {
+    sessionStorage.setItem("auth", authValue.role)
+    console.log("sessionStorage", sessionStorage.getItem("auth"))
+  }
 }
 
 const resetAuth = () => {
