a7c32468ac02aa455bc211f258fffde2598b4135
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-13 21:45:43 +0100

    add connexion

--- src/routes/connexion/+page.svelte
+++ src/routes/connexion/+page.svelte
@@ -1,7 +1,13 @@
 <script>
-  import {resetAuth, setAuth, authStore} from "$lib/Auth/auth"
+  //import {resetAuth, setAuth, authStore} from "$lib/Auth/auth"
+  import { onMount } from "svelte"
   import Navbar from "$lib/Components/Navbar.svelte";
-  $: auth = ($authStore != "")
+  //$: auth
+  let connected;
+
+  onMount(()=>{
+    //auth = ($authStore != "")
+  })
 
   function logout(){
     resetAuth()
@@ -14,27 +20,39 @@
   let password
   /** @type {boolean} */
   let rememberMe = false // TODO : true => localStorage / false => sessionStorage
-  function login(){
-    setAuth(email)// TODO take the appropriate field
-    console.log("FIELDS : ", email, password, rememberMe)
+  async function login(){
+
+    //setAuth(email)// TODO take the appropriate field
+    connected = await fetch("/api/utilisateurs", {
+      method:"POST",
+      body:JSON.stringify({identifiant:email, mot_de_passe:password}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    password = ''
+    console.log("connect", connected)
   }
 </script>
 
 <Navbar /> <!--TODO au lieu de mettre dans chaque pages, le mettre UNE fois dans le +- main-->
 <form on:submit|preventDefault={login}>
-  <label for="email">Email</label>
-  <input type="email" id="email" bind:value={email} required>
+  <label>Login
+    <input type="text" bind:value={email} required>
+  </label>
 
-  <label for="password">Mot de passe</label>
-  <input type="password" id="password" bind:value={password} required>
+  <label>Mot de passe
+  <input type="password" bind:value={password} required>
+  </label>
 
-  <label for="rememberMe">Se souvenir de moi</label>
-  <input type="checkbox" id="rememberMe" bind:checked={rememberMe}>
+  <label>Se souvenir de moi
+  <input type="checkbox" bind:checked={rememberMe}>
+  </label>
 
   <button type="submit">Connexion</button>
 </form>
 
-{#if auth}
+{#if password}
   <button on:click={ logout }>Se déconnecter</button>
 {/if}
 <button on:click={() => history.back() }>Retour</button>
