f6ab8a7159ebc061cbf3ef70a4b942fd6ad93d58
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-13 13:51:58 +0100

    refactor extract api call

--- src/routes/commandes/[commande]/+page.svelte
+++ src/routes/commandes/[commande]/+page.svelte
@@ -1,90 +1,68 @@
 <script>
   import { onMount } from "svelte";
-  import { readable } from "svelte/store";
+  import { page } from "$app/stores";
   import Navbar from "$lib/Components/Navbar.svelte";
-  import { getRequest } from "@sveltejs/kit/node";
+
+  const idCommande = $page.params.commande;
+  let commande = [];
+
   let role = "livreur";
-  let commande;
   let responseUpdatedCommande;
 
   var updatedCommande = {
-  "ordre": 1,
-  "statut": "en attente",
-  "lignes_commande": [
-    {
-      "id_commande": 3,
-      "id_article": 1,
-      "nb_caisses": 69,
-      "nb_unites": 0
-    },
-    {
-      "id_commande": 3,
-      "id_article": 2,
-      "nb_caisses": 69,
-      "nb_unites": 0
-    },
-    {
-      "id_commande": 3,
-      "id_article": 3,
-      "nb_caisses": 69,
-      "nb_unites": 0
-    },
-    {
-      "id_commande": 3,
-      "id_article": 4,
-      "nb_caisses": 69,
-      "nb_unites": 0
-    },
-    {
-      "id_commande": 3,
-      "id_article": 5,
-      "nb_caisses": 69,
-      "nb_unites": 0
-    },
-    {
-      "id_commande": 3,
-      "id_article": 6,
-      "nb_caisses": 69,
-      "nb_unites": 0  
-    }
-  ]
-};
+    ordre: 1,
+    statut: "en attente",
+    lignes_commande: [
+      {
+        id_commande: 3,
+        id_article: 1,
+        nb_caisses: 69,
+        nb_unites: 0,
+      },
+      {
+        id_commande: 3,
+        id_article: 2,
+        nb_caisses: 69,
+        nb_unites: 0,
+      },
+      {
+        id_commande: 3,
+        id_article: 3,
+        nb_caisses: 69,
+        nb_unites: 0,
+      },
+      {
+        id_commande: 3,
+        id_article: 4,
+        nb_caisses: 69,
+        nb_unites: 0,
+      },
+      {
+        id_commande: 3,
+        id_article: 5,
+        nb_caisses: 69,
+        nb_unites: 0,
+      },
+      {
+        id_commande: 3,
+        id_article: 6,
+        nb_caisses: 69,
+        nb_unites: 0,
+      },
+    ],
+  };
 
   /**
    * @type {Object}
    */
   onMount(async () => {
-    //TODO attention pour l'instant hardcodage de id car pas de page COMMANDES, faire que quand on clique sur une commande pour la modifier
-    // on rajoute dans le localStrorage
-    const idCommande = sessionStorage.getItem('idCommande');
     await getCommande(idCommande);
-
-    console.log("id recup ", idCommande);
   });
 
-
-
   async function getCommande(idCommande) {
-    try {
-      const response = await fetch(`http://localhost:9000/commandes/${idCommande}`);
-
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP: ${response.status}`);
-      }
-
-      commande = await response.json();
-     
-      
-     
-  updatedCommande=commande;
-  
-      console.log("commande " , commande);
-      console.log("updatedCommande : ", updatedCommande);
-    } catch (error) {
-      console.error("Erreur lors de la récupération de la commande:", error);
-    }
+    const response = await fetch(`/api/commandes/${idCommande}`);
+    commande = await response.json();
   }
-  
 
   function displayRequest(updatedCommande, idCommande) {
     const requestPayload = {
@@ -109,17 +87,15 @@
     // Appelez la fonction updateCommande pour envoyer réellement la requête
     updateCommande(updatedCommande, idCommande);
   }
-   /**
+  /**
    * modification d'une commande.
    */
-   async function updateCommande(updatedCommande, recu) {
+  async function updateCommande(updatedCommande, recu) {
     try {
       //POURQUOI L'idCOMMANDE EST NULL ALORS QU4ON LE PRENDS DANS LE SESSIONSTORE ??
       //console.log("l'id de la commande est", idCommande);
       //aussi hardcodé car pas de localStore avec l'id dedans
       const response = await fetch(
-
-
         `http://localhost:9000/commandes/2/modifier`,
         {
           method: "PUT",
@@ -135,11 +111,11 @@
                 //ligne.id_article undefined
                 id_article: 1,
                 new_nb_caisses: ligne.nb_caisses,
-                new_nb_unites: ligne.nb_caisses
-              })
+                new_nb_unites: ligne.nb_caisses,
+              }),
             ),
           }),
-        }
+        },
       );
       if (!response.ok) {
         throw new Error(`Erreur HTTP: ${response.status}`);
@@ -151,7 +127,6 @@
       console.error("Erreur lors de la modification de la commande:", error);
     }
   }
-
 
   function saveChanges() {
     // Ajoutez ici la logique pour sauvegarder les modifications
@@ -172,43 +147,44 @@
   };
 </script>
 
-<body>
-  <div class="container">
-    <Navbar />
-
-    <div class="tab-infos">
-      <div class="left-column">
-        <ul>
-          <label for="tournee"> Tournee : </label>
-          <span id="tournee"
-            >{commande?.tournee?.nom ?? commande?.tournee?.id_tournee ?? ""}
-          </span>
-        </ul>
-        <ul>
-          <label for="creche"> Creche : </label>
-          <span id="creche"> {commande?.creche?.nom ?? commande?.creche?.id_creche ?? ""}</span>
-        </ul>
-      </div>
-      <div class="right-column">
-        <ul>
-          <label for="date"> Date : </label>
-          <span id="ordre"> {commande ? commande.ordre : ""}</span>
-
-          <!--<span id="date"> {commande ? commande.tournee.date : " "}</span>-->
-        </ul>
-        <ul>
-          <label for="ordre"> Ordre : </label>
-          <span id="ordre"> {commande ? commande.ordre : ""}</span>
-        </ul>
-        <ul>
-          <label for="statut"> Statut : </label>
-          <span id="statu"> {commande ? commande.statut : ""}</span>
-        </ul>
-      </div>
+<Navbar />
+<div class="container">
+  <div class="tab-infos">
+    <div class="left-column">
+      <ul>
+        <label for="tournee"> Tournee : </label>
+        <span id="tournee"
+          >{commande?.tournee?.nom ?? commande?.tournee?.id_tournee}
+        </span>
+      </ul>
+      <ul>
+        <label for="creche"> Creche : </label>
+        <span id="creche">
+          {commande?.creche?.nom ?? commande?.creche?.id_creche}</span
+        >
+      </ul>
     </div>
+    <div class="right-column">
+      <ul>
+        <label for="date"> Date : </label>
+        <span id="ordre"> {commande ? commande.ordre : ""}</span>
 
-    <div class="data-table">
-      {#if commande && commande.statut !=="terminée"  }
+        <!--<span id="date"> {commande ? commande.tournee.date : " "}</span>-->
+      </ul>
+      <ul>
+        <label for="ordre"> Ordre : </label>
+        <span id="ordre"> {commande ? commande.ordre : ""}</span>
+      </ul>
+      <ul>
+        <label for="statut"> Statut : </label>
+        <span id="statu"> {commande ? commande.statut : ""}</span>
+      </ul>
+    </div>
+  </div>
+
+  <!--
+  <div class="data-table">
+    {#if commande && commande.statut !== "terminée"}
       {#if commande && commande.lignes_commande}
         <table>
           <thead>
@@ -219,14 +195,12 @@
             </tr>
           </thead>
 
-          
-
           <tbody>
             {#each commande.lignes_commande as ligne, index (index)}
               {#if ligne !== undefined}
                 <tr key={index}>
                   <td>
-                    {ligne.article.libelle }{ligne.article.taille || ""}
+                    {ligne.article.libelle}{ligne.article.taille || ""}
                   </td>
                   <td>
                     <input
@@ -247,40 +221,34 @@
             {/each}
             <button
               on:click={() => /*todo modifier la commande*/ {
-                console.log("la nouvelle commande est : " ,updatedCommande);
+                console.log("la nouvelle commande est : ", updatedCommande);
                 displayRequest(updatedCommande, 0);
               }}
             >
               Update
             </button>
           </tbody>
-
-          <!-- ... (reste du code) ... -->
         </table>
       {:else}
-        <p>Aucune ligne de commande disponible.</p>
+        <p>Aucune ligne de commande disponible.</p>--><!--Ne devrais pas arriver en prod--><!--
       {/if}
     {:else}
-        <p> cette commande a deja été livree, impossible de la modifier</p>
-      {/if}
-    </div>
-    {#if role !== "livreur"}
-      <div class="buttons">
-        
-        <button on:click={() => saveChanges()}> Sauvegarder </button>
-        <button on:click={() => cancelChanges()}> Annuler</button>
-      </div>
+      <p>cette commande a deja été livree, impossible de la modifier</p>
     {/if}
   </div>
-</body>
+  -->
 
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Formulaire avec mise en page</title>
-    <style>
-    table {
+  {#if role !== "livreur"}
+    <div class="buttons">
+      <button on:click={() => saveChanges()}> Sauvegarder </button>
+      <button on:click={() => cancelChanges()}> Annuler</button>
+    </div>
+  {/if}
+</div>
+
+<style>
+
+  table {
     border-collapse: collapse;
     width: 400px;
     margin: 20px;
@@ -307,6 +275,7 @@
   tr:nth-child(even) {
     background-color: #f2f2f2;
   }
+
   .buttons {
     display: flex;
     justify-content: space-around; /* Ajustement de l'espacement entre les boutons */
@@ -314,36 +283,18 @@
     margin-top: 20px; /* Ajout d'une marge en haut des boutons */
   }
 
-    body {
-      font-family: Arial, sans-serif;
-      display: flex;
-      justify-content: center;
-      height: 100vh;
-      align-items:center
-      margin: 0;
-    }
+  .tab-infos {
+    display: flex;
+    width: 400px;
+  }
 
-    .tab-infos {
-      display: flex;
-      
-      width: 400px;
-    }
+  .left-column, .right-column {
+    width: 100%;
+    margin-top: 10px; /* Ajout d'une marge entre les colonnes */
+  }
 
-    .left-column, .right-column {
-      width: 100%;
-      margin-top: 10px; /* Ajout d'une marge entre les colonnes */
-
-
-    }
-    
-
-    .date-input {
-      width: 100%;
-    }
-    label, span {
-      display: inline-block;
-      margin-bottom: 8px;
-    }
-    </style>
-  </head>
-</html>
+  label, span {
+    display: inline-block;
+    margin-bottom: 8px;
+  }
+</style>
