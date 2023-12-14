180a9738e6b98edbb44c563bcd9a132c48455f84
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-14 17:35:45 +0100

    add POST

--- src/routes/api/commandes/[commande]/+server.ts
+++ src/routes/api/commandes/[commande]/+server.ts
@@ -2,6 +2,22 @@ import { json as jsonResponse } from "@sveltejs/kit";
 import { host } from "$lib/Api/config";
 
 const apiRoute = "commandes";
+
+async function createTournees(id: string) {
+  try {
+    const reponse = await fetch(`${host}/${apiRoute}/${id}`);
+    if (reponse.ok) {
+      const json = await reponse.json();
+      return jsonResponse(json)
+    } else {
+      throw new Error(reponse.statusText);
+    }
+  } catch (error) {
+    console.log("error in commandesParDefaut/tourneesParDefaut/[id]/+server.ts", error) // TODO handle error
+    return jsonResponse(null)
+  }
+}
+
 async function getCommandesTournee(id: string) {
   try {
     const response = await fetch(`${host}/${apiRoute}/${id}`);
@@ -20,4 +36,8 @@ async function getCommandesTournee(id: string) {
 
 export async function GET({params}) {
   return getCommandesTournee(params.commande);
+}
+
+export async function POST({params}) {
+  return createTournees(params.commande);
 }
