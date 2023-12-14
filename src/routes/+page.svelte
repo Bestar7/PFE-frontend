3eb2e260be0bd16b3a2e91402af482569061d30e
Joachim Bastin Local <joachim.bastin@gmail.com>
2023-12-14 12:21:08 +0100

    test install btn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,48 @@
 <script>
     import { goto } from "$app/navigation";
     import { onMount } from "svelte";
-    import { getAuth } from "$lib/Auth/auth";
-
-    onMount(()=>{
-        console.log("getAuth()", getAuth())
+    import { getAuth, roles } from "$lib/Auth/auth";
+    
+    /*
+    onMount(() => {
+        console.log("getAuth()", getAuth());
         if (!getAuth())
-            goto("/connexion")
-        else if (getAuth() == "livreur")
-            goto("/tournees") // TODO choisir la 'page d'acceuil' d'un livreur
-        else if (getAuth() == "admin")
-            goto("/tournees") // TODO choisir la 'page d'acceuil' d'un admin
-    })
+            goto("/connexion");
+        else if (getAuth() == roles.livreur)
+            goto("/tournees"); // TODO choisir la 'page d'acceuil' d'un livreur
+        else if (getAuth() == roles.admin) goto("/tournees"); // TODO choisir la 'page d'acceuil' d'un admin
+    });
+    */
+
+
+
+    let deferredPrompt;
+
+    onMount(() => {
+        window.addEventListener("beforeinstallprompt", (event) => {
+            // Prevent the mini-infobar from appearing on mobile
+            event.preventDefault();
+            // Stash the event so it can be triggered later.
+            deferredPrompt = event;
+        })}
+    )
+
+    // Trigger install when your custom button is clicked
+    function install() {
+        // Show the install prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+                console.log("User accepted the install prompt");
+            } else {
+                console.log("User dismissed the install prompt");
+            }
+            // Reset deferredPrompt for the next install prompt
+            deferredPrompt = null;
+        });
+    }
 </script>
 
 <h1>ERROR</h1>
+<button on:click={install}>Install App</button>
